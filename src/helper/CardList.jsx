47f371a1c0e8192b47b9c6cd3b54d58be62ec28e
WongLick <163981891+WongLick@users.noreply.github.com>
2024-04-21 16:29:07 +0800

    fixed how data is fetched
    - able to display 8 Card components

--- src/helper/CardList.jsx
+++ src/helper/CardList.jsx
@@ -1,41 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../components/Card";
 import "../styles/CardList.css";
 import geodude from "../assets/geodude.png";
 
 function CardList() {
   const URL = "https://pokeapi.co/api/v2/pokemon";
-  const ID = Math.round(Math.random() * 10);
+  const [pokemon, setPokemon] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchPokemon = async () => {
-      fetch(`${URL}/${ID}`)
-        .then((response) => response.json())
-        .then((data) => {
-          renderCard(data);
-        });
+    const fetchPokemons = async () => {
+      setError(null);
+      setIsLoading(true);
+      const randomPokemons = [];
+      try {
+        for (let i = 0; i < 8; i++) {
+          const randomID = Math.floor(Math.random() * 150) + 1;
+          const response = await fetch(`${URL}/${randomID}`);
+          if (!response.ok) {
+            throw new Error("Failed to fetch data");
+          }
+          const data = await response.json();
+          randomPokemons.push({
+            name: data.name,
+            image: data.sprites.front_default,
+          });
+        }
+        setPokemon(randomPokemons);
+      } catch (error) {
+        console.error("Fetch data failed.", error);
+        setError("Failed to fetch data. Please try again.");
+      }
+      setIsLoading(false);
     };
-    fetchPokemon();
+    fetchPokemons();
   }, []);
 
-  function renderCard(data) {
-    const name = data.species.name;
-    const image = data.sprites.front_default;
-    console.log(data);
-    console.log(name);
-    console.log(image);
-    return <Card name={name} image={image} />;
+  if (isLoading) {
+    return <div className="cardGrid">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="cardGrid">{error}</div>;
   }
 
   return (
     <div className="cardGrid">
-      <Card name="Geodude" image={geodude} />
-      <Card name={2} image={geodude} />
-      <Card name={3} image={geodude} />
-      <Card name={4} image={geodude} />
-      <Card name={5} image={geodude} />
-      <Card name={6} image={geodude} />
-      <Card name={7} image={geodude} />
+      {pokemon.map((pokemon, index) => (
+        <Card key={index} name={pokemon.name} image={pokemon.image} />
+      ))}
     </div>
   );
 }
